f73d579b7ee1e95b185d11e6780d6ccd98083f9b
runhem <alexandra@ep.se>
2016-05-18 17:14:07 +0200

    whohooo

--- app/js/boilCtrl.js
+++ app/js/boilCtrl.js
@@ -7,32 +7,52 @@ eggApp.controller('boilCtrl', ['$scope','$timeout', function($scope,$timeout){
 // Klar med boil -> Vill du spara?
 
 // Till CSS sen! 
-	small = document.querySelector("#smallEgg");
-	large = document.querySelector("#mediumEgg");
-// Klart med tilll CSS
+// 	small = document.querySelector("#smallEgg");
+// 	large = document.querySelector("#mediumEgg");
+// // Klart med tilll CSS
 
-	medium = document.querySelector("#largeEgg");
+// 	medium = document.querySelector("#largeEgg");
 
-	small.style.display = "none";
-	large.style.display = "none";
+// 	small.style.display = "none";
+// 	large.style.display = "none";
 
-	$scope.changeSize = function(size){
-	  if(size == 'small'){
-	    medium.style.display = "none";
-	    large.style.display = "none";
-	    small.style.display = "block";
-	  }
-	  if(size == 'medium'){
-	    small.style.display = "none";
-	    large.style.display = "none";
-	    medium.style.display = "block";
+// 	$scope.changeSize = function(size){
+// 	  if(size == 'small'){
+// 	    medium.style.display = "none";
+// 	    large.style.display = "none";
+// 	    small.style.display = "block";
+// 	  }
+// 	  if(size == 'medium'){
+// 	    small.style.display = "none";
+// 	    large.style.display = "none";
+// 	    medium.style.display = "block";
 
-	  }
-	  if(size == 'large'){
-	    small.style.display = "none";
-	    medium.style.display = "none";
-	    large.style.display = "block";
-	  }
-	};
+// 	  }
+// 	  if(size == 'large'){
+// 	    small.style.display = "none";
+// 	    medium.style.display = "none";
+// 	    large.style.display = "block";
+// 	  }
+// 	};
+
+	$scope.counter = 10;
+    var stopped;
+
+	$scope.countdown = function() {
+	    stopped = $timeout(function() {
+	        if($scope.counter>0){
+	     $scope.counter--;   
+	     $scope.countdown(); 
+	     }
+	     else{
+	        $scope.counter=0;
+	     }  
+	    }, 1000);
+	  };	
+
+
+
+
+	  
 
 }]);
