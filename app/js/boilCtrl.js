35d4321fd2790b663a3bcd2598e156e7917ae0c8
Frida <friekl@kth.se>
2016-05-18 16:24:56 +0200

    newegghej

--- app/js/boilCtrl.js
+++ app/js/boilCtrl.js
@@ -4,6 +4,35 @@ eggApp.controller('boilCtrl', ['$scope','$timeout', function($scope,$timeout){
 // Slider ska generera tid
 // Countdown
 // Start boil
-// Klar med boil -> Vill du spara? 
+// Klar med boil -> Vill du spara?
+
+// Till CSS sen! 
+	small = document.querySelector("#smallEgg");
+	large = document.querySelector("#mediumEgg");
+// Klart med tilll CSS
+
+	medium = document.querySelector("#largeEgg");
+
+	small.style.display = "none";
+	large.style.display = "none";
+
+	$scope.changeSize = function(size){
+	  if(size == 'small'){
+	    medium.style.display = "none";
+	    large.style.display = "none";
+	    small.style.display = "block";
+	  }
+	  if(size == 'medium'){
+	    small.style.display = "none";
+	    large.style.display = "none";
+	    medium.style.display = "block";
+
+	  }
+	  if(size == 'large'){
+	    small.style.display = "none";
+	    medium.style.display = "none";
+	    large.style.display = "block";
+	  }
+	};
 
 }]);
