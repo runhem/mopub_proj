86cf0994cae469891870d046327680c90b6dbc52
runhem <alexandra@ep.se>
2016-05-19 17:02:27 +0200

    egg egg mera egg

--- app/js/boilCtrl.js
+++ app/js/boilCtrl.js
@@ -7,33 +7,33 @@ eggApp.controller('boilCtrl', ['$scope','$timeout', function($scope,$timeout){
 // Klar med boil -> Vill du spara?
 
 // Till CSS sen! 
-	small = document.querySelector("#smallEgg");
-	large = document.querySelector("#mediumEgg");
-// Klart med tilll CSS
+// 	small = document.querySelector("#smallEgg");
+// 	large = document.querySelector("#mediumEgg");
+// // Klart med tilll CSS
 
-	medium = document.querySelector("#largeEgg");
+// 	medium = document.querySelector("#largeEgg");
 
-	small.style.display = "none";
-	large.style.display = "none";
+// 	small.style.display = "none";
+// 	large.style.display = "none";
 
-	$scope.changeSize = function(size){
-	  if(size == 'small'){
-	    medium.style.display = "none";
-	    large.style.display = "none";
-	    small.style.display = "block";
-	  }
-	  if(size == 'medium'){
-	    small.style.display = "none";
-	    large.style.display = "none";
-	    medium.style.display = "block";
+// 	$scope.changeSize = function(size){
+// 	  if(size == 'small'){
+// 	    medium.style.display = "none";
+// 	    large.style.display = "none";
+// 	    small.style.display = "block";
+// 	  }
+// 	  if(size == 'medium'){
+// 	    small.style.display = "none";
+// 	    large.style.display = "none";
+// 	    medium.style.display = "block";
 
-	  }
-	  if(size == 'large'){
-	    small.style.display = "none";
-	    medium.style.display = "none";
-	    large.style.display = "block";
-	  }
-	};
+// 	  }
+// 	  if(size == 'large'){
+// 	    small.style.display = "none";
+// 	    medium.style.display = "none";
+// 	    large.style.display = "block";
+// 	  }
+// 	};
 
 
 	$scope.saveEgg = function(){
@@ -45,12 +45,14 @@ eggApp.controller('boilCtrl', ['$scope','$timeout', function($scope,$timeout){
 	$scope.timerRunning = false;
 	$scope.wannaSave = false;
 
+
+
 	$scope.startTimer = function (){
 	     $scope.$broadcast('timer-start');
          $scope.timerRunning = true;
          TweenMax.to('.animateEgg',2,{y:"+=400px"});
 		 TweenMax.to('.animateText',0.01,{'opacity':'0'});
-		 document.getElementById('audio').play(egg);
+		 document.getElementById('audio1').play();
          };
 
 
@@ -59,6 +61,7 @@ eggApp.controller('boilCtrl', ['$scope','$timeout', function($scope,$timeout){
 		$scope.$apply();
 		$scope.timerRunning = false;
 		TweenLite.to('#finishText', 0.001, {text:{value:"TAKE OUT YOUR EGG!", delimiter:" "}, ease:Linear.easeNone, 'color':'red', 'font-weight':'bold'});
+		TweenMax.to('#audio1', 2, {'volume':0}, "-=1");
 	});        
 
 /*
