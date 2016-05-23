2291cb7061756cf9d73704269b760e0223e46b3e
runhem <alexandra@ep.se>
2016-05-23 10:33:42 +0200

    fsdfsd

--- app/js/timerCtrl.js
+++ app/js/timerCtrl.js
@@ -27,47 +27,8 @@ eggApp.controller('timerCtrl', function($scope,$timeout,$location,eggModel,$wind
    //       TweenMax.to('.animateEgg',2,{y:"+=400px"});
 		 // TweenMax.to('.animateText',0.01,{'opacity':'0'});
 		
-		var music = document.getElementById('audio1');
-		music.play();
-
-		var t1 = new TimelineMax();
-
-		 //>>>>> Eventuellt ha i databas??? 
-		var tipsList = ["Tip #1: Put your egg in a sock and spin it before boiling and you will have a scramled egg!",
-		 				"Tip #2: Be gentle with your eggs. They break very easy!",
-		 				"Tip #3: Try putting an egg on your next hamburger! Yummie!",
-		 				"Tip #4: Eggs stay fresh longer if your store them in the fridge!",
-		 				"Tip #5: Putting a spoon in the pot while boiling help the egg not to break!", 
-		 				"Tip #6: Put salt in the water when boiling to prevent the egg to spread if it breaks"]
-		
-		//animates tips in loop depending on how many tips there are in tipsList-array
-		for(i=1; i<tipsList.length+2; i++){
-		 	t1.from('#t'+i, 0.1, {css:{'height':'0px', 'width':'5'}});
-		 	t1.to("#t"+i,6,{css:{'height':'100px'}});
-			t1.to("#c"+i,2,{css:{'height':'40px','width':'40px', 'border-radius':'50%','opacity':'1'}});
-		 	t1.to("#text"+i,0.001,{text:{value:tipsList[i-1], delimiter:" "},ease:Linear.easeNone});
-		 }
-    };
-	$scope.stopTimer = function (){
-		$scope.$broadcast('timer-stop');
-		$scope.timerRunning = false;
-		};
-
- 	$scope.operation = 'start';
-
-    $scope.startStop = function(){
-
-    document.getElementById('timer')[$scope.operation]();
-    $scope.operation = ($scope.operation === 'start' || $scope.operation === 'resume') ? 'stop' : 'resume';
-    if($scope.operation === 'stop'){
-    	$scope.animate = true;
-    	$scope.t1.paused(false);
-         
-		
 		$scope.music = document.getElementById('audio1');
 		$scope.music.play();
-
-		
 
 		 //>>>>> Eventuellt ha i databas??? 
 		var tipsList = ["Tip #1: Put your egg in a sock and spin it before boiling and you will have a scramled egg!",
@@ -85,16 +46,18 @@ eggApp.controller('timerCtrl', function($scope,$timeout,$location,eggModel,$wind
 		 	$scope.t1.to("#text"+i,0.001,{text:{value:tipsList[i-1], delimiter:" "},ease:Linear.easeNone});
 		 }
     };
-    if($scope.operation === 'resume'){
-    	$scope.t1.paused(true)
-    	$scope.music.pause();
-    }
-  }
+	$scope.stopTimer = function (){
+		$scope.$broadcast('timer-stop');
+		$scope.timerRunning = false;
+		};
+
 
 	$scope.$on('timer-stopped', function (event, data){
 		console.log("inne i timer stopped")
 		$scope.wannaSave = true;
+		$scope.music.pause();
 		$scope.$apply();
+		
 		TweenLite.to('#finishText', 0.001, {text:{value:"YOUR EGG IS DONE!", delimiter:" "}, ease:Linear.easeNone});
 		TweenMax.to('#audio1', 2, {'volume':0}, "-=1");
 		TweenMax.to('.tipsQueue',1,{opacity:0});
